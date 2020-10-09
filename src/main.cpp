cea45d3fcd61300215b9caed887d557530b9c7b3
Christos Gkantidis <christosg8@gmail.com>
2020-10-09 11:20:53 +0200

    Refactor sum_vecs into a function

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
 #include <vector>
 
+std::vector<int> sum_vecs(std::vector<int> const &vec1, std::vector<int> const &vec2) {
+  size_t size = vec1.size();
+
+  std::vector<int> vec3(size);
+  for (int i = 0; i < size; ++i) {
+    vec3[i] = vec1[i] + vec2[i];
+  }
+
+  return vec3;
+}
+
 int main() {
   std::vector<int> vec1{1, 2, 3, 4, 5};
   std::vector<int> vec2{6, 7, 8, 9, 10};
-  std::vector<int> vec3(5);
-
-  for (int i = 0; i < 5; ++i) {
-    vec3[i] = vec1[i] + vec2[i];
-  }
+  std::vector<int> vec3 = sum_vecs(vec1, vec2);
 
   for (int i = 0; i < 5; ++i) {
     std::cout << vec3[i] << "\n";
